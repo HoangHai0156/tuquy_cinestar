b851d2634331a24de38c4174334cea3bb8a12931
akova88 <phuoc.fiit@gmail.com>
2023-09-06 13:52:13 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Schedules from './components/Schedules/Schedules';
+import Home from './components/home';
 
 function App() {
   return (
-    <>
-    </>
+    // <Schedules />
+    <Home />
   );
 }
 
