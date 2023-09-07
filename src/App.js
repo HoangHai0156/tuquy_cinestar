17e70a1f2303687db443c2fc45bc27357c689615
akova88 <phuoc.fiit@gmail.com>
2023-09-07 08:49:54 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Schedules from "./components/Schedules/Schedules";
-import Header from "./components/Header/Header";
+
 import User from "./components/Header/User";
 import Slider from "./components/Header/Slider";
-import Promotion from "./components/Header/Promotion";
-import PlayingMovies from "./components/Header/PlayingMovies";
-
 import ContentPage from './components/Schedules/ContentPage';
 import Footer from "./components/Footer/Footer";
 import ShowingMovies from "./components/ShowingMovies/ShowingMovies";
