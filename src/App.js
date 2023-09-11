9f94c6453b517c7be0abe3115bae5fecb8085b6e
nguyen <hoangvannguyen23011996@gmail.com>
2023-09-11 08:53:26 +0700

    introduce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
-
-import User from "./components/Header/User";
-import Slider from "./components/Header/Slider";
-import ContentPage from './components/Schedules/ContentPage';
+// import User from "./components/Header/User";
+// import Slider from "./components/Header/Slider";
+// import ContentPage from './components/Schedules/ContentPage';
 import Footer from "./components/Footer/Footer";
-import ShowingMovies from "./components/ShowingMovies/ShowingMovies";
+// import ShowingMovies from "./components/ShowingMovies/ShowingMovies";
+// import Introduce from "./components/Introduce/Introduce";
+import SliderSlick from "./components/Introduce/SliderSlick";
+import SliderPlayingMovie from "./components/Introduce/SliderPlayingMovie";
 
 function App() {
   return (
     <>
       {/* <Header /> */}
-      <User />
-      <Slider />
-      <ShowingMovies />
-      <ContentPage />
+      {/* <User /> */}
+      {/* <Slider /> */}
+      {/* <ShowingMovies /> */}
+      {/* <ContentPage /> */}
+      {/* <Introduce /> */}
+      <SliderSlick />
+      <SliderPlayingMovie />
       <Footer />
     </>
-
-
-
   );
 }
 
